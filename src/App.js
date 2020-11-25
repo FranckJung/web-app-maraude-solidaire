43b5034533d888a3ef3e89698fb0ad3b00c9c6bc
Franck <franckjung@yahoo.fr>
2020-11-25 18:49:01 +0100

    debut projet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Contact from "./Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <HashRouter>
+      <nav>
+        <NavLink to="/">Logo</NavLink>
+        <ul>
+          <li>
+            <NavLink to="#">L'association</NavLink>
+          </li>
+          <li>
+            <NavLink to="#">pas d'idées</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/View/Contact.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+
+class Contact extends Component {
+    render(){
+        return (
+            <div>
+                prout
+            </div>
+        );
+    }
+}
+
+export default Contact;
